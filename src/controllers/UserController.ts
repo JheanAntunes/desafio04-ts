1aa1b8a568c6e6d0d07f881afccffde1c4af2118
Jhean Antunes Lopes <brizadaojhean@gmail.com>
2024-12-20 14:57:34 -0300

    feat: add validate field email

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,28 +1,34 @@
-import { Request, Response } from 'express'
-import { UserService } from '../services/UserService'
+import { Request, Response } from "express";
+import { UserService } from "../services/UserService";
 
 export class UserController {
-    userService: UserService
+  userService: UserService;
 
-    constructor(
-        userService = new UserService()
-    ){
-        this.userService = userService
+  constructor(userService = new UserService()) {
+    this.userService = userService;
+  }
+
+  createUser = (request: Request, response: Response): Response => {
+    const user = request.body;
+
+    if (!user.name) {
+      return response
+        .status(400)
+        .json({ message: "Bad request! Name obrigatório" });
     }
 
-    createUser = (request: Request, response: Response): Response => {
-        const user = request.body
-
-        if(!user.name){
-            return response.status(400).json({ message: 'Bad request! Name obrigatório'})
-        }
-
-        this.userService.createUser(user.name, user.email)
-        return response.status(201).json({ message: 'Usuário criado'})
+    if (!user.email) {
+      return response
+        .status(400)
+        .json({ message: "Bad request! Email obrigatório" });
     }
 
-    getAllUsers = (request: Request, response: Response) => {
-        const users = this.userService.getAllUsers()
-        return response.status(200).json( users )
-    } 
+    this.userService.createUser(user.name, user.email);
+    return response.status(201).json({ message: "Usuário criado" });
+  };
+
+  getAllUsers = (request: Request, response: Response) => {
+    const users = this.userService.getAllUsers();
+    return response.status(200).json(users);
+  };
 }
