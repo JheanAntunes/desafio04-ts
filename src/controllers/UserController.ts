41ca5ca478877b1a3dfe3261eb87860902b92663
Jhean Antunes Lopes <brizadaojhean@gmail.com>
2024-12-20 15:08:05 -0300

    feat: add method deleteUser

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -27,6 +27,19 @@ export class UserController {
     return response.status(201).json({ message: "Usuário criado" });
   };
 
+  deleteUser = (request: Request, response: Response) => {
+    // TODO
+    const user = request.body;
+    if (!user.email) {
+      return response
+        .status(400)
+        .json({ message: "Bad request! Email obrigatório" });
+    }
+    this.userService.deleteUser(user.email);
+
+    return response.status(200).json({ message: "Usuário deletado" });
+  };
+
   getAllUsers = (request: Request, response: Response) => {
     const users = this.userService.getAllUsers();
     return response.status(200).json(users);
--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,36 +1,38 @@
 export interface User {
-    name: string
-    email: string
+  name: string;
+  email: string;
 }
 
 const db = [
-    {
-        name: "Joana",
-        email: "joana@dio.com",
-    }
-]
+  {
+    name: "Joana",
+    email: "joana@dio.com",
+  },
+];
 
 export class UserService {
-    db: User[]
+  db: User[];
 
-    constructor(
-        database = db
-    ){
-        this.db = database
-    }
+  constructor(database = db) {
+    this.db = database;
+  }
 
-    createUser = (name: string, email: string) => {
-        const user = {
-            name,
-            email
-        }
+  createUser = (name: string, email: string) => {
+    const user = {
+      name,
+      email,
+    };
 
-        this.db.push(user)
-        console.log('DB atualizado', this.db)
-    }
+    this.db.push(user);
+    console.log("DB atualizado", this.db);
+  };
 
-    getAllUsers = () => {
-        return this.db
-    }
+  deleteUser = (email: string) => {
+    const filteredUsers = this.db.filter((user) => user.email !== email);
+    this.db = filteredUsers;
+  };
+
+  getAllUsers = () => {
+    return this.db;
+  };
 }
-
