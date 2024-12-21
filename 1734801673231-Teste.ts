dd3462a82da1302b3a1e952a5e5b6c5cf7367fb7
Jhean Antunes Lopes <brizadaojhean@gmail.com>
2024-12-21 14:28:20 -0300

    feat: add migration class Teste1734801673231 and initial SQLite database file

---
+++ 1734801673231-Teste.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class Teste1734801673231 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
