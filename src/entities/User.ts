b679049873165fbfdb564beb0585241ad7731f05
Jhean Antunes Lopes <brizadaojhean@gmail.com>
2024-12-23 12:08:09 -0300

    refactor: rename user_id to id_user in User entity for consistency

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -3,7 +3,7 @@ import { randomUUID } from "crypto";
 @Entity("users")
 export class User {
   @PrimaryGeneratedColumn()
-  user_id: string;
+  id_user: string;
 
   @Column({ nullable: false })
   name: string;
@@ -15,7 +15,7 @@ export class User {
   password: string;
 
   constructor(name: string, email: string, password: string) {
-    this.user_id = randomUUID();
+    this.id_user = randomUUID();
     this.name = name;
     this.email = email;
     this.password = password;
