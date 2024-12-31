3316300d460dce3b824bab59d8b398fbf1a7abeb
Jhean Antunes Lopes <brizadaojhean@gmail.com>
2024-12-31 12:21:50 -0300

    feat: update user retrieval route to use user ID as a parameter

--- src/routes.ts
+++ src/routes.ts
@@ -8,6 +8,6 @@ const userController = new UserController();
 const loginController = new LoginController();
 
 router.post("/user", userController.createUser);
-router.get("/user", userController.getUser);
+router.get("/user/:id", userController.getUser);
 router.delete("/user", userController.deleteUser);
 router.post("/login", loginController.login);
