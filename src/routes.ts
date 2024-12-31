253e47e15e8d99f382d5c00da729522a03a58972
Jhean Antunes Lopes <brizadaojhean@gmail.com>
2024-12-31 11:40:16 -0300

    feat: add login route with LoginController for user authentication

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,13 @@
 import { Router, Request, Response } from "express";
 import { UserController } from "./controllers/UserController";
+import { LoginController } from "./controllers/login-controller";
 
 export const router = Router();
 
 const userController = new UserController();
+const loginController = new LoginController();
 
 router.post("/user", userController.createUser);
 router.get("/user", userController.getUser);
 router.delete("/user", userController.deleteUser);
+router.post("/login", loginController.login);
